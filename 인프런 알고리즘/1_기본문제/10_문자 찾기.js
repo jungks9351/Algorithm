ec0b59919c82db741bde77d33f5946fe2711a0fc
kimjungjin <jungks9351@gmail.com>
2022-01-17 22:02:53 +0900

    remove : 인프런 알고리즘

--- 인프런 알고리즘/1_기본문제/10_문자 찾기.js
+++
@@ -1,7 +0,0 @@
-function solution(s, t) {
-  let result = s.split(t).length;
-  return result - 1;
-}
-
-let str = 'COMPUTERPROGRAMMING';
-console.log(solution(str, 'R'));
--- 인프런 알고리즘/1_기본문제/4_1부터 n까지의 합.js
+++
@@ -1,9 +0,0 @@
-function solution(n) {
-  let sum = 0;
-  for (let i = 1; i <= n; i++) {
-    sum += i;
-  }
-  return sum;
-}
-
-console.log(solution(10));
