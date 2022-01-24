e0c4c5365db2028d531894f2ae1bbbd9a53025cb
kimjungjin <jungks9351@gmail.com>
2022-01-25 00:45:34 +0900

    feat : 역순 연결 리스트

--- 파이썬 알고리즘 인터뷰/알고리즘/8장 연결 리스트/15.reverse-linked-list.py
+++ 파이썬 알고리즘 인터뷰/알고리즘/8장 연결 리스트/15.reverse-linked-list.py
@@ -16,3 +16,13 @@ def reverseList(slef, head: ListNode) -> ListNode:
         return reverse(next, node)
 
     return reverse(head)
+
+# 반복 구조로 뒤집기
+def reverseList2(self, head: ListNode) -> ListNode:
+    node, prev = head, None
+
+    while node:
+        next, node.next = node.next, prev
+        prev, node = node, next
+
+    return prev
