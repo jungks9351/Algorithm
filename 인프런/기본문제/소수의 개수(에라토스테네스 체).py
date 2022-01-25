01872faf97cef9b70b465a78b127e9031ea9be38
kimjungjin <jungks9351@gmail.com>
2022-01-26 02:05:02 +0900

    feat : 인프런 기본문제 소수의 개수(에라토스테네스 체)

---
+++ 인프런/기본문제/소수의 개수(에라토스테네스 체).py
@@ -0,0 +1,15 @@
+# 소수(에라토스테네스 체)
+
+# n = int(input())
+
+n = 20
+ch = [0]*(n+1)
+cnt = 0
+
+for i in range(2, n+1):
+    if ch[i] == 0:
+        cnt +=1
+        for j in range(i, n+1, i):
+            ch[j] = 1
+
+print(cnt)
