0d1c255abff51903ad817b80e1b0ea80417f9021
kimjungjin <jungks9351@gmail.com>
2022-01-26 19:00:41 +0900

    feat : 인프런 탐색&시뮬레이션 수들의 합

---
+++ 인프런/탐색 & 시뮬레이션/수들의 합.py
@@ -0,0 +1,28 @@
+n, m = 8, 3
+
+a = [1,2,1,3,1,1,1,2]
+
+# n, m = map(int,input().split())
+# a = list(map(int, input().split()))
+
+lt = 0
+rt = 1
+tot = a[0]
+cnt = 0
+
+while True:
+    if tot < m:
+        if rt < n:
+            tot += a[rt]
+            rt += 1
+        else:
+            break
+    elif tot == m:
+        cnt+= 1
+        tot -= a[lt]
+        lt += 1
+    else:
+        tot -= a[lt]
+        lt += 1
+
+print(cnt)
