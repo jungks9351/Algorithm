12c7b91f97179e5167d016acc080961103b93551
kimjungjin <jungks9351@gmail.com>
2022-01-26 19:00:04 +0900

    feat : 인프런 탐색&시뮬레이션 두 리스트 합치기

---
+++ 인프런/탐색 & 시뮬레이션/두 리스트 합치기.py
@@ -0,0 +1,27 @@
+# 두 리스트 합치기
+
+# n =int(input())
+# a = list(map(int, input().split()))
+# m =int(input())
+# b = list(map(int, input().split()))
+
+n = 3
+a = [1,3, 5]
+m = 5
+b = [2,3,6,7,9]
+p1 = p2 = 0
+c = []
+
+while p1 < n and p2 < m:
+    if a[p1] <= b[p2]:
+        c.append(a[p1])
+        p1 +=1
+    else :
+        c.append(b[p2])
+        p2 +=1
+if p1 < n:
+    c = c + a[p1:]
+if p2 < m:
+    c = c + b[p2:]
+
+print(c)
