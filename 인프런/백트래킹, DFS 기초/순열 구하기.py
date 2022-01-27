c36c1ba689b17595aba3bfd32abf8a6e96200eed
kimjungjin <jungks9351@gmail.com>
2022-01-27 15:33:18 +0900

    feat : 인프런 DFS 순열 구하기

---
+++ 인프런/백트래킹, DFS 기초/순열 구하기.py
@@ -0,0 +1,29 @@
+import sys
+
+sys.stdin = open('input.text')
+
+
+def DFS(x):
+    global cnt
+    if x == m:
+        for j in range(m):
+            print(res[j], end=" ")
+        print()
+        cnt += 1
+    else:
+        for i in range(1, n + 1):
+            if ch[i] == 0:
+                ch[i] =1
+                res[x] =i
+                DFS(x+1)
+                ch[i] =0
+
+if __name__ == '__main__':
+    n, m = map(int, input().split())
+
+    res = [0]*n
+    ch = [0]*(n + 1)
+
+    cnt = 0
+    DFS(0)
+    print(cnt)
