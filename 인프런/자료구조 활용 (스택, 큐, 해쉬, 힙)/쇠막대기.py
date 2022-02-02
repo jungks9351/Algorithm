06eaad03cebe9fc319aacb23dfb1e73321ae314c
kimjungjin <jungks9351@gmail.com>
2022-02-02 20:37:21 +0900

    feat : 인프런 스택 쇠막대기

---
+++ 인프런/자료구조 활용 (스택, 큐, 해쉬, 힙)/쇠막대기.py
@@ -0,0 +1,21 @@
+import sys
+
+sys.stdin = open('input.text')
+
+s = input()
+
+stack = []
+
+cnt = 0
+
+for i in range(len(s)):
+    if s[i] == '(':
+        stack.append(s[i])
+    else:
+        stack.pop()
+        if s[i-1] == '(':
+            cnt += len(stack)
+        else:
+            cnt +=1
+
+print(cnt)
